8b0dde6ebd6af8964d6d9990363fd7c7c2ca6fb5
kimji <dnfl42clsrn4@hanmail.net>
2019-02-15 23:29:37 +0900

    난수 발생시 중복되는 데이터를 없애기 위해 기존 배열에 바로 난수를 저장하는 방식에서 HashSet을 적용해 중복되는 데이터를 없애고 얻은 데이터를 배열에 저장하는 방법으로 수정함

--- app/src/main/java/kim/com/test/lottopicker/RandomPicker.java
+++ app/src/main/java/kim/com/test/lottopicker/RandomPicker.java
@@ -2,25 +2,33 @@ package kim.com.test.lottopicker;
 
 import android.util.Log;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomPicker {
-    int[] lottoNumberList;
-    final int LIST_SIZE = 7;
+    private int[] lottoNumberList;
+    private final int LIST_SIZE = 7;
     public RandomPicker(){
         lottoNumberList = new int[LIST_SIZE];
     }
     public void makeLottoNumberList(){
-        int i;
         Random random = new Random();
         long seed = System.currentTimeMillis();
         random.setSeed(seed);
-        for(i=0; i< LIST_SIZE; i++){
-            lottoNumberList[i] = random.nextInt(46);
-            Log.d("Random List", ""+i+": "+lottoNumberList[i]);
+        Set<Integer> lottoNumberSet = new HashSet<>();
+        int i;
+        for(i=0; lottoNumberSet.size()< LIST_SIZE; i++){
+            lottoNumberSet.add((random.nextInt(45)+1));//(0~44)+1
         }
-    }
-    private void duplicatedLottoNumberCheck(){
+        i=0;
+        Iterator<Integer> iterator = lottoNumberSet.iterator();
+        while (iterator.hasNext()){
+          lottoNumberList[i] = iterator.next();
+          Log.d("Random List", ""+i+": "+lottoNumberList[i]);
+          i++;
+        }
 
     }
     public int[] getLottoNumberList() {
